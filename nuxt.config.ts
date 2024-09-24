eefee21f1cce7d50e803a597903bfd93ff97c19a
Nico <72673830+NicolasHrt@users.noreply.github.com>
2024-09-25 00:14:14 +0200

    feat: ajout de nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   extends: ['@nuxt/ui-pro'],
-  modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/image']
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/image','@nuxthq/studio']
 })
